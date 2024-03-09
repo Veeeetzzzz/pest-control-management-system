8d6ed2e38cd09a04cf745e4ea508b7d53d60f540
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-03-09 22:29:50 +0000

    Update config.tsx

--- src/components/auth/config.tsx
+++ src/components/auth/config.tsx
@@ -1,4 +1,4 @@
-import { Configuration, PopupRequest } from "@azure/msal-browser";
+import { Configuration, PopupRequest, PublicClientApplication } from "@azure/msal-browser";
 
 export const msalConfig: Configuration = {
   auth: {

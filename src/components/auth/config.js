b8658fceb8fdfafe6e22c7715757458a59d34a20
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-03-09 22:11:26 +0000

    Update config.js

--- src/components/auth/config.js
+++ src/components/auth/config.js
@@ -1,4 +1,6 @@
-export const msalConfig = {
+import { Configuration, PopupRequest } from "@azure/msal-browser";
+
+export const msalConfig: Configuration = {
   auth: {
     clientId: "e089d7bc-ed2c-4100-b1ca-475f764262a4", // Your client ID
     authority: "https://login.microsoftonline.com/d7e1ee4c-3a16-49a5-b078-a456b6169744", // Your authority
@@ -10,6 +12,6 @@ export const msalConfig = {
   },
 };
 
-export const loginRequest = {
-  scopes: ["User.Read"]
+export const loginRequest: PopupRequest = {
+  scopes: ["User.Read"],
 };

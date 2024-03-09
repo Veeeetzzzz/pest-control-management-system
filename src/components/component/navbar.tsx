d461595bdf3ffbf87d48403cbdabadd438c72025
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-03-09 19:48:07 +0000

    Update navbar.tsx

--- src/components/component/navbar.tsx
+++ src/components/component/navbar.tsx
@@ -52,8 +52,7 @@ export function Navbar() {
   )
 }
 
-
-function SearchIcon(props) {
+function SearchIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
@@ -70,11 +69,10 @@ function SearchIcon(props) {
       <circle cx="11" cy="11" r="8" />
       <path d="m21 21-4.3-4.3" />
     </svg>
-  )
+  );
 }
 
-
-function BellIcon(props) {
+function BellIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
@@ -91,5 +89,5 @@ function BellIcon(props) {
       <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
       <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
     </svg>
-  )
+  );
 }

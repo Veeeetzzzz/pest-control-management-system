194efd6cfbe88cfa7d5ff5c3d4cde6d7521ae597
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-03-10 17:47:08 +0000

    Update config.tsx

--- src/components/auth/config.tsx
+++ src/components/auth/config.tsx
@@ -4,7 +4,7 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: "e089d7bc-ed2c-4100-b1ca-475f764262a4", // Your client ID
     authority: "https://login.microsoftonline.com/d7e1ee4c-3a16-49a5-b078-a456b6169744", // Your authority
-    redirectUri: "pest-control-management-system.vercel.app", // This should match your redirect URI in Azure AD registration
+    redirectUri: "https://pest-control-management-system.vercel.app", // This should match your redirect URI in Azure AD registration
   },
   cache: {
     cacheLocation: "sessionStorage",

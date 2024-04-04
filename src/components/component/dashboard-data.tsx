1117c957eac76738739dbbbe8edc6c5b40f75b64
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-04-04 21:10:59 +0100

    Create dashboard-data.tsx

---
+++ src/components/component/dashboard-data.tsx
@@ -0,0 +1,3 @@
+//This component connects to the PG DB and gets the data required for the dashboard view
+//Make sure you import /auth/postgres-db-auth
+//Make sure you update dashboard.tsx and import component/dashboard-data

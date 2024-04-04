971a54529ed139b5f7615d03f829e97130eb22a2
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-04-04 21:08:53 +0100

    Create postgres-db-auth.tsx

---
+++ src/components/auth/postgres-db-auth.tsx
@@ -0,0 +1,3 @@
+//This component will handle the inital DB connection for the application
+//It must be imported into each component
+//It's currently only calling the DB once per componment load - i.e. dashboard has one query to display the data

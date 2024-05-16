aaff72696ba8fa4de9fe0a5b3733b9aaefa571dc
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-05-16 22:05:07 +0100

    Update dashboard-data.tsx

--- src/components/component/dashboard-data.tsx
+++ src/components/component/dashboard-data.tsx
@@ -1,8 +1,12 @@
 //This component connects to the PG DB and gets the data required for the dashboard view
 //Make sure you import /auth/postgres-db-auth
 //Make sure you update dashboard.tsx and import component/dashboard-data
+
+import { NextApiRequest, NextApiResponse } from 'next';
 import { Pool } from 'pg';
 
+
+//This is just for testing - connection strings will stored in .env files for production
 const pool = new Pool({
   // Configure your PostgreSQL connection details
   host: 'your-host',
@@ -13,7 +17,7 @@ const pool = new Pool({
 });
 
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const result = await pool.query('SELECT * FROM your_table');
     res.status(200).json(result.rows);
